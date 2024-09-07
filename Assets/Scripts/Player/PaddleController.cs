2b25b870589e46e9e51ee7cb9a6d9b0ea9d57421
Merve <merve_demir@stu.fsm.edu.tr>
2024-09-07 20:45:04 +0300

    Prevent Paddle from Crossing Game Boundary When It Grow & Reorganize PaddleController

--- Assets/Scripts/Player/PaddleController.cs
+++ Assets/Scripts/Player/PaddleController.cs
@@ -3,38 +3,66 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour
 {
 
-    public float maxPosition;
-
+    private Camera mainCamera;
+    private float maxPosition;
+    private float minPosition;
+    private float halfPaddleWidth;
 
     void Start()
     {
+        mainCamera = Camera.main;
 
-
+        maxPosition = 6.8f;
+        minPosition = -6.8f;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
 
+        UpdatePaddlePosition();
+
+        ClampPaddlePosition();
+    }
+
+
+    private void UpdatePaddlePosition()
+    {
         Vector3 mousePosition = Input.mousePosition;
 
         // Mouse pozisyonunun Unity sahnesindeki dünya koordinatlarýný hesaplama
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
+        mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
 
         // Paddle position
         Vector3 newPos = transform.position;
-
         newPos.x = mousePosition.x;
-
-        // Paddle hareketini sýnýrlandýrmýþ olduk
-        newPos.x = Mathf.Clamp(newPos.x, -maxPosition, maxPosition);
-
-        // Paddle new position
         transform.position = newPos;
-
-
     }
+
+
+    public void ClampPaddlePosition()
+    {
+        Vector3 paddlePosition = transform.position;
+
+        halfPaddleWidth = transform.localScale.x / 2;
+
+        // Sað sýnýr kontrolü
+        if (paddlePosition.x + halfPaddleWidth > maxPosition)
+        {
+            paddlePosition.x = maxPosition - halfPaddleWidth;
+        }
+
+        // Sol sýnýr kontrolü
+        if (paddlePosition.x - halfPaddleWidth < minPosition)
+        {
+            paddlePosition.x = minPosition + halfPaddleWidth;
+        }
+
+        transform.position = paddlePosition;
+    }
+
+
+
 
 
 }
